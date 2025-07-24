f09f26c198ce02166fbf9444935f25d624416b17
root <root@ip-10-10-132-79.ec2.internal>
2025-07-24 02:10:28 +0000

    added

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -23,7 +23,7 @@
                 type="submit"
                 class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
             >
-            </button>
+            </button> 추가
         </form>
 
         <!-- 할 일 목록 -->
