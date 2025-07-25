7d93fae5f38d10950265d6483eaeeaf2dfd82e32
rincher <pillarofthelight@gmail.com>
2025-07-25 16:16:09 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,7 @@ SOURCE_DIR=/tmp/deploy-source
 
 # 1. 덮어쓰기
 sudo cp -rf $SOURCE_DIR/* $APP_DIR/
-sudo chown -R ec2-user:ec2-user $APP_DIR
+sudo chown -R apache:apache $APP_DIR
 
 # 2. Laravel 설정
 cd $APP_DIR
@@ -15,7 +15,6 @@ php artisan route:cache
 php artisan view:cache
 
 # 3. 퍼미션
-sudo chown -R apache:apache $APP_DIR/storage $APP_DIR/bootstrap/cache
 sudo chmod -R 775 $APP_DIR/storage $APP_DIR/bootstrap/cache
 
 # ✅ 4. 임시 배포 소스 삭제
